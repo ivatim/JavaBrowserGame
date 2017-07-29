e5d61de84149f3f96853242848679b27e9f8c5a5
ivatim <ivantimkin92@mail.ru>
2017-07-29 16:44:05 +0300

    Added Overrides and type="password"

--- src/main/java/service/websocket/WebSocketServiceImpl.java
+++ src/main/java/service/websocket/WebSocketServiceImpl.java
@@ -9,18 +9,22 @@ import java.util.Map;
 public class WebSocketServiceImpl implements WebSocketService {
     private Map<String, GameWebSocket> userSockets = new HashMap<>();
 
+    @Override
     public void addPlayer(GameWebSocket user) {
         userSockets.put(user.getMyName(), user);
     }
 
+    @Override
     public void notifyMyNewScore(Player user) {
         userSockets.get(user.getMyName()).setMyScore(user);
     }
 
+    @Override
     public void notifyEnemyNewScore(Player user) {
         userSockets.get(user.getMyName()).setEnemyScore(user);
     }
 
+    @Override
     public void notifyStartGame(Player user) {
         GameWebSocket gameWebSocket = userSockets.get(user.getMyName());
         gameWebSocket.startGame(user);
